9f1977dbf3b4bbda8eb7467004df58b41981f5ad
Bo Li <drluodian@gmail.com>
2023-04-12 15:17:23 +0000

    add coco-caption to git tree

---
+++ LAVIS/coco-caption/README.md
@@ -0,0 +1,55 @@
+Microsoft COCO Caption Evaluation
+===================
+
+Evaluation codes for MS COCO caption generation.
+
+## Requirements ##
+- java 1.8.0
+- python 2.7
+
+## Files ##
+./
+- cocoEvalCapDemo.py (demo script)
+
+./annotation
+- captions_val2014.json (MS COCO 2014 caption validation set)
+- Visit MS COCO [download](http://mscoco.org/dataset/#download) page for more details.
+
+./results
+- captions_val2014_fakecap_results.json (an example of fake results for running demo)
+- Visit MS COCO [format](http://mscoco.org/dataset/#format) page for more details.
+
+./pycocoevalcap: The folder where all evaluation codes are stored.
+- evals.py: The file includes COCOEavlCap class that can be used to evaluate results on COCO.
+- tokenizer: Python wrapper of Stanford CoreNLP PTBTokenizer
+- bleu: Bleu evalutation codes
+- meteor: Meteor evaluation codes
+- rouge: Rouge-L evaluation codes
+- cider: CIDEr evaluation codes
+- spice: SPICE evaluation codes
+
+## Setup ##
+
+- You will first need to download the [Stanford CoreNLP 3.6.0](http://stanfordnlp.github.io/CoreNLP/index.html) code and models for use by SPICE. To do this, run:
+    ./get_stanford_models.sh
+
+## References ##
+
+- [Microsoft COCO Captions: Data Collection and Evaluation Server](http://arxiv.org/abs/1504.00325)
+- PTBTokenizer: We use the [Stanford Tokenizer](http://nlp.stanford.edu/software/tokenizer.shtml) which is included in [Stanford CoreNLP 3.4.1](http://nlp.stanford.edu/software/corenlp.shtml).
+- BLEU: [BLEU: a Method for Automatic Evaluation of Machine Translation](http://www.aclweb.org/anthology/P02-1040.pdf)
+- Meteor: [Project page](http://www.cs.cmu.edu/~alavie/METEOR/) with related publications. We use the latest version (1.5) of the [Code](https://github.com/mjdenkowski/meteor). Changes have been made to the source code to properly aggreate the statistics for the entire corpus.
+- Rouge-L: [ROUGE: A Package for Automatic Evaluation of Summaries](http://anthology.aclweb.org/W/W04/W04-1013.pdf)
+- CIDEr: [CIDEr: Consensus-based Image Description Evaluation] (http://arxiv.org/pdf/1411.5726.pdf)
+- SPICE: [SPICE: Semantic Propositional Image Caption Evaluation] (http://panderson.me/images/SPICE.pdf)
+
+## Developers ##
+- Xinlei Chen (CMU)
+- Hao Fang (University of Washington)
+- Tsung-Yi Lin (Cornell)
+- Ramakrishna Vedantam (Virgina Tech)
+
+## Acknowledgement ##
+- David Chiang (University of Norte Dame)
+- Michael Denkowski (CMU)
+- Alexander Rush (Harvard University)
---
+++ LAVIS/coco-caption/pycocoevalcap/eval.py
@@ -0,0 +1,75 @@
+__author__ = 'tylin'
+from tokenizer.ptbtokenizer import PTBTokenizer
+from bleu.bleu import Bleu
+from meteor.meteor import Meteor
+from rouge.rouge import Rouge
+from cider.cider import Cider
+from spice.spice import Spice
+
+class COCOEvalCap:
+    def __init__(self, coco, cocoRes, df):
+        self.evalImgs = []
+        self.eval = {}
+        self.imgToEval = {}
+        self.coco = coco
+        self.cocoRes = cocoRes
+        self.params = {'image_id': coco.getImgIds()}
+        self.df = df
+
+    def evaluate(self):
+        imgIds = self.params['image_id']
+        # imgIds = self.coco.getImgIds()
+        gts = {}
+        res = {}
+        for imgId in imgIds:
+            gts[imgId] = self.coco.imgToAnns[imgId]
+            res[imgId] = self.cocoRes.imgToAnns[imgId]
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print 'tokenization...'
+        tokenizer = PTBTokenizer()
+        gts  = tokenizer.tokenize(gts)
+        res = tokenizer.tokenize(res)
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print 'setting up scorers...'
+        scorers = [
+            (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
+            (Meteor(),"METEOR"),
+            (Rouge(), "ROUGE_L"),
+            (Cider(df=self.df), "CIDEr"),
+            (Spice(), "SPICE")
+        ]
+        # =================================================
+        # Compute scores
+        # =================================================
+        for scorer, method in scorers:
+            print 'computing %s score...'%(scorer.method())
+            score, scores = scorer.compute_score(gts, res)
+            if type(method) == list:
+                for sc, scs, m in zip(score, scores, method):
+                    self.setEval(sc, m)
+                    self.setImgToEvalImgs(scs, gts.keys(), m)
+                    print "%s: %0.3f"%(m, sc)
+            else:
+                self.setEval(score, method)
+                self.setImgToEvalImgs(scores, gts.keys(), method)
+                print "%s: %0.3f"%(method, score)
+        self.setEvalImgs()
+
+    def setEval(self, score, method):
+        self.eval[method] = score
+
+    def setImgToEvalImgs(self, scores, imgIds, method):
+        for imgId, score in zip(sorted(imgIds), scores):
+            if not imgId in self.imgToEval:
+                self.imgToEval[imgId] = {}
+                self.imgToEval[imgId]["image_id"] = imgId
+            self.imgToEval[imgId][method] = score
+
+    def setEvalImgs(self):
+        self.evalImgs = [self.imgToEval[imgId] for imgId in sorted(self.imgToEval.keys())]
