c74f345d0d338855dab1e1872c09e0519f49979a
Bo Li <drluodian@gmail.com>
2023-04-19 07:23:20 +0000

    update demo

--- demo_flamingo.py
+++ demo_flamingo.py
@@ -14,6 +14,7 @@ from open_flamingo import create_model_and_transforms
 from huggingface_hub import hf_hub_download
 import torch
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Demo")
     parser.add_argument("--lm_path", required=True, help="path to language model")
@@ -22,9 +23,7 @@ def parse_args():
     parser.add_argument(
         "--options",
         nargs="+",
-        help="override some settings in the used config, the key-value pair "
-        "in xxx=yyy format will be merged into config file (deprecate), "
-        "change to --cfg-options instead.",
+        help="override some settings in the used config, the key-value pair " "in xxx=yyy format will be merged into config file (deprecate), " "change to --cfg-options instead.",
     )
     args = parser.parse_args()
     return args
@@ -43,11 +42,7 @@ def setup_seeds(config):
 
 def initialize_model(lm_path, cross_attn_every_n_layers, checkpoint_path=None):
     model, image_processor, tokenizer = create_model_and_transforms(
-        clip_vision_encoder_path="ViT-L-14",
-        clip_vision_encoder_pretrained="openai",
-        lang_encoder_path=lm_path,
-        tokenizer_path=lm_path,
-        cross_attn_every_n_layers=cross_attn_every_n_layers
+        clip_vision_encoder_path="ViT-L-14", clip_vision_encoder_pretrained="openai", lang_encoder_path=lm_path, tokenizer_path=lm_path, cross_attn_every_n_layers=cross_attn_every_n_layers
     )
 
     model.to("cuda")
@@ -58,6 +53,7 @@ def initialize_model(lm_path, cross_attn_every_n_layers, checkpoint_path=None):
     msg = model.load_state_dict(torch.load(checkpoint_path), strict=False)
     print(msg)
     return model, image_processor, tokenizer
+
 
 # ========================================
 #             Model Initialization
@@ -70,30 +66,33 @@ model, image_processor, tokenizer = initialize_model(lm_path=args.lm_path, cross
 #             Gradio Setting
 # ========================================
 
+
 def gradio_reset(chat_state):
     chat_state.messages = []
-    return None, gr.update(value=None, interactive=True), gr.update(placeholder='Please upload your image first', interactive=False),gr.update(value="Upload & Start Chat", interactive=True), chat_state
+    return None, gr.update(value=None, interactive=True), gr.update(placeholder="Please upload your image first", interactive=False), gr.update(value="Upload & Start Chat", interactive=True), chat_state
 
-def upload_and_anwer(image_1, image_2, image_3, 
-                                        text_1, text_2, text_3,
-                                        num_beams):
+
+def upload_and_anwer(image_1, image_2, image_3, text_1, text_2, text_3, num_beams):
     vision_x = [image_processor(image_1).unsqueeze(0), image_processor(image_2).unsqueeze(0), image_processor(image_3).unsqueeze(0)]
     vision_x = torch.cat(vision_x, dim=0)
     vision_x = vision_x.unsqueeze(1).unsqueeze(0)
-    tokenizer.padding_side = "left" # For generation padding tokens should be on the left
+    vision_x = vision_x.to("cuda")
+    tokenizer.padding_side = "left"  # For generation padding tokens should be on the left
     lang_x = tokenizer(
         [f"<image>{text_1}<|endofchunk|><image>{text_2}<|endofchunk|><image>{text_3}"],
         return_tensors="pt",
     )
+    lang_x = {k: v.to("cuda") for k, v in lang_x.items()}
     generated_tokens = model.generate(
-    vision_x=vision_x,
-    lang_x=lang_x["input_ids"],
-    attention_mask=lang_x["attention_mask"],
-    max_new_tokens=20,
-    num_beams=num_beams,
+        vision_x=vision_x,
+        lang_x=lang_x["input_ids"],
+        attention_mask=lang_x["attention_mask"],
+        max_new_tokens=20,
+        num_beams=num_beams,
     )
     generated_text = tokenizer.decode(generated_tokens[0])
     return generated_text
+
 
 title = """<header>
 <style>
@@ -114,21 +113,12 @@ with gr.Blocks() as demo:
     gr.Markdown(title)
     with gr.Row():
         with gr.Column(scale=0.5):
-            image_1 = gr.Image(type="pil", value=Image.open(requests.get(
-        "http://images.cocodataset.org/val2017/000000039769.jpg", 
-        stream=True
-    ).raw), label="Image_1")
-            text_1 = gr.Textbox(label='text_1', placeholder='An image of two cats.')
-            image_2 = gr.Image(type="pil", value=Image.open(requests.get(
-        "http://images.cocodataset.org/test-stuff2017/000000028137.jpg", 
-        stream=True
-    ).raw), label="Image_2")
-            text_2 = gr.Textbox(label='text_2', placeholder='An image of a bathroom sink.')
-            image_3 = gr.Image(type="pil", value=Image.open(requests.get(
-        "http://images.cocodataset.org/test-stuff2017/000000028352.jpg", 
-        stream=True
-    ).raw), label="Image_3")
-            text_3 = gr.Textbox(label='text_3', placeholder='An image of')
+            image_1 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/val2017/000000039769.jpg", stream=True).raw), label="Image_1")
+            text_1 = gr.Textbox(label="text_1", placeholder="An image of two cats.")
+            image_2 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/test-stuff2017/000000028137.jpg", stream=True).raw), label="Image_2")
+            text_2 = gr.Textbox(label="text_2", placeholder="An image of a bathroom sink.")
+            image_3 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/test-stuff2017/000000028352.jpg", stream=True).raw), label="Image_3")
+            text_3 = gr.Textbox(label="text_3", placeholder="An image of")
             upload_button = gr.Button(value="Upload & Start", interactive=True, variant="primary")
             clear = gr.Button("Restart")
 
@@ -141,11 +131,15 @@ with gr.Blocks() as demo:
                     interactive=True,
                     label="beam search numbers",
                 )
-                
-    upload_button.click(upload_and_anwer, [image_1, image_2, image_3, 
-                                        text_1, text_2, text_3,
-                                        num_beams
-                                     ], outputs=["text"],
+
+        with gr.Column():
+            output_text = gr.Textbox(label='User', placeholder='Ask me anything here')
+
+            
+    upload_button.click(
+        upload_and_anwer,
+        [image_1, image_2, image_3, text_1, text_2, text_3, num_beams],
+        outputs=output_text,
     )
 
     # clear.click(gradio_reset, [chat_state], [chatbot, images, text_input, upload_button, chat_state], queue=False)
