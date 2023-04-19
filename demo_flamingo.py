79aa561769daac957c0fce9685c0abf7d96073e8
Bo Li <drluodian@gmail.com>
2023-04-19 19:35:05 +0000

    update flamingo demo

--- demo_flamingo.py
+++ demo_flamingo.py
@@ -12,13 +12,11 @@ import gradio as gr
 from collie_core.collie_chat.chat import Chat, CONV_LANG, CONV_VISION
 from open_flamingo import create_model_and_transforms
 from huggingface_hub import hf_hub_download
-import torch
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Demo")
     parser.add_argument("--lm_path", required=True, help="path to language model")
-    parser.add_argument("--checkpoint_path", help="path to best collie checkpoint, default to OpenFlamingo-9B")
+    parser.add_argument("--checkpoint_path", help="path to best collie checkpoint, default to original OpenFlamingo-9B")
     parser.add_argument("--cross_attn_every_n_layers", type=int, default=4, help="cross attention every n layers")
     parser.add_argument(
         "--options",
@@ -29,51 +27,33 @@ def parse_args():
     return args
 
 
-def setup_seeds(config):
-    seed = config.run_cfg.seed
-
+def initialize_model(args):
+    seed = 42
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
 
     cudnn.benchmark = False
     cudnn.deterministic = True
-
-
-def initialize_model(lm_path, cross_attn_every_n_layers, checkpoint_path=None):
     model, image_processor, tokenizer = create_model_and_transforms(
-        clip_vision_encoder_path="ViT-L-14", clip_vision_encoder_pretrained="openai", lang_encoder_path=lm_path, tokenizer_path=lm_path, cross_attn_every_n_layers=cross_attn_every_n_layers
+        clip_vision_encoder_path="ViT-L-14", clip_vision_encoder_pretrained="openai", lang_encoder_path=args.lm_path, tokenizer_path=args.lm_path, cross_attn_every_n_layers=args.cross_attn_every_n_layers
     )
 
     model.to("cuda")
     model.eval()
 
-    if checkpoint_path is None:
-        checkpoint_path = hf_hub_download("openflamingo/OpenFlamingo-9B", "checkpoint.pt")
-    msg = model.load_state_dict(torch.load(checkpoint_path), strict=False)
+    if args.checkpoint_path is None:
+        args.checkpoint_path = hf_hub_download("openflamingo/OpenFlamingo-9B", "checkpoint.pt")
+    msg = model.load_state_dict(torch.load(args.checkpoint_path), strict=False)
     print(msg)
     return model, image_processor, tokenizer
 
-
-# ========================================
-#             Model Initialization
-# ========================================
-
 args = parse_args()
-model, image_processor, tokenizer = initialize_model(lm_path=args.lm_path, cross_attn_every_n_layers=args.cross_attn_every_n_layers, checkpoint_path=args.checkpoint_path)
+model, image_processor, tokenizer = initialize_model(args)
 
-# ========================================
-#             Gradio Setting
-# ========================================
-
-
-def gradio_reset(chat_state):
-    chat_state.messages = []
-    return None, gr.update(value=None, interactive=True), gr.update(placeholder="Please upload your image first", interactive=False), gr.update(value="Upload & Start Chat", interactive=True), chat_state
-
-
-def upload_and_anwer(image_1, image_2, image_3, text_1, text_2, text_3, num_beams):
+def upload_and_anwer(image_1, image_2, image_3, text_1, text_2, text_3, max_new_tokens, num_beams, temperature, topk, top_p, no_repeat_ngram_size, length_penalty, num_return_sequences, do_sample, early_stopping):
     vision_x = [image_processor(image_1).unsqueeze(0), image_processor(image_2).unsqueeze(0), image_processor(image_3).unsqueeze(0)]
+    # vision_x = [image_processor(image_3).unsqueeze(0)]
     vision_x = torch.cat(vision_x, dim=0)
     vision_x = vision_x.unsqueeze(1).unsqueeze(0)
     vision_x = vision_x.to("cuda")
@@ -82,17 +62,29 @@ def upload_and_anwer(image_1, image_2, image_3, text_1, text_2, text_3, num_beam
         [f"<image>{text_1}<|endofchunk|><image>{text_2}<|endofchunk|><image>{text_3}"],
         return_tensors="pt",
     )
+    do_sample = True if do_sample == 1 else False
+    early_stopping = True if early_stopping == 1 else False
     lang_x = {k: v.to("cuda") for k, v in lang_x.items()}
+
     generated_tokens = model.generate(
         vision_x=vision_x,
         lang_x=lang_x["input_ids"],
         attention_mask=lang_x["attention_mask"],
-        max_new_tokens=20,
+        max_new_tokens=max_new_tokens,
         num_beams=num_beams,
+        temperature=temperature,
+        top_k=topk,
+        top_p=top_p,
+        no_repeat_ngram_size=no_repeat_ngram_size,
+        prefix_allowed_tokens_fn=None,
+        length_penalty=length_penalty,
+        num_return_sequences=num_return_sequences,
+        do_sample=do_sample,
+        early_stopping=early_stopping,
     )
     generated_text = tokenizer.decode(generated_tokens[0])
-    return generated_text
-
+    post_process_text = generated_text.split("<image>")[-1]
+    return post_process_text
 
 title = """<header>
 <style>
@@ -112,33 +104,36 @@ h1 {text-align: center;}
 with gr.Blocks() as demo:
     gr.Markdown(title)
     with gr.Row():
-        with gr.Column(scale=0.5):
-            image_1 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/val2017/000000039769.jpg", stream=True).raw), label="Image_1")
-            text_1 = gr.Textbox(label="text_1", placeholder="An image of two cats.")
-            image_2 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/test-stuff2017/000000028137.jpg", stream=True).raw), label="Image_2")
-            text_2 = gr.Textbox(label="text_2", placeholder="An image of a bathroom sink.")
-            image_3 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/test-stuff2017/000000028352.jpg", stream=True).raw), label="Image_3")
-            text_3 = gr.Textbox(label="text_3", placeholder="An image of")
-            upload_button = gr.Button(value="Upload & Start", interactive=True, variant="primary")
-            clear = gr.Button("Restart")
-
-            with gr.Accordion("Advanced options", open=False):
-                num_beams = gr.Slider(
-                    minimum=1,
-                    maximum=16,
-                    value=3,
-                    step=1,
-                    interactive=True,
-                    label="beam search numbers",
-                )
-
         with gr.Column():
-            output_text = gr.Textbox(label='User', placeholder='Ask me anything here')
+            image_1 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/val2017/000000039769.jpg", stream=True).raw), label="Image_1")
+            text_1 = gr.Textbox(label="text_1", value="Outputs: two cats sleeping.")
+        with gr.Column():
+            image_2 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/test-stuff2017/000000028137.jpg", stream=True).raw), label="Image_2")
+            text_2 = gr.Textbox(label="text_2", value="Outputs: a bathroom sink.")
+        with gr.Column():
+            image_3 = gr.Image(type="pil", value=Image.open(requests.get("http://images.cocodataset.org/test-stuff2017/000000028352.jpg", stream=True).raw), label="Image_3")
+            text_3 = gr.Textbox(label="text_3", value="Outputs: ")
 
-            
+    upload_button = gr.Button(value="Upload & Start", interactive=True, variant="primary", scale=0.2)
+    output_text = gr.Textbox(label='Output', placeholder="Please upload your image first", multiline=True, interactive=False, scale=0.8)
+
+
+    with gr.Row():
+        with gr.Accordion("Advanced options", open=True):
+            max_new_tokens = gr.Slider(minimum=20, maximum=200, value=30, step=10, interactive=True, label="number of tokens to generate")
+            num_beams = gr.Slider(minimum=1, maximum=16, value=3, step=1, interactive=True, label="beam search numbers")
+            temperature = gr.Slider(minimum=0, maximum=1, value=0.7, step=0.1, interactive=True, label="temperature")
+            topk = gr.Slider(minimum=0, maximum=10, value=1, step=0.1, interactive=True, label="topk")
+            top_p = gr.Slider(minimum=0, maximum=1, value=1.0, step=0.1, interactive=True, label="top_p")
+            no_repeat_ngram_size = gr.Slider(minimum=1, maximum=10, value=1, step=1, interactive=True, label="no_repeat_ngram_size")
+            length_penalty = gr.Slider(minimum=1, maximum=5, value=1, step=0.1, interactive=True, label="length_penalty")
+            num_return_sequences = gr.Slider(minimum=1, maximum=10, value=1, step=1, interactive=True, label="num_return_sequences")
+            do_sample = gr.Slider(minimum=0, maximum=1, value=1, step=1, interactive=True, label="do_sample")
+            early_stopping = gr.Slider(minimum=0, maximum=1, value=1, step=1, interactive=True, label="early_stopping")
+
     upload_button.click(
-        upload_and_anwer,
-        [image_1, image_2, image_3, text_1, text_2, text_3, num_beams],
+        fn=upload_and_anwer,
+        inputs=[image_1, image_2, image_3, text_1, text_2, text_3, max_new_tokens, num_beams, temperature, topk, top_p, no_repeat_ngram_size, length_penalty, num_return_sequences, do_sample, early_stopping],
         outputs=output_text,
     )
 
