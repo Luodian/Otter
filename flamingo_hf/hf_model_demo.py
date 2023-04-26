56654da6d331ed32e494a72fe97d3fbe7be69de8
Bo Li <drluodian@gmail.com>
2023-04-26 06:00:14 +0000

    stash changes

--- flamingo_hf/hf_model_demo.py
+++ flamingo_hf/hf_model_demo.py
@@ -13,18 +13,18 @@ from modeling_flamingo import FlamingoModel
 # config = FlamingoConfig.from_json_file("./flamingo_hf/config.json")
 # model = FlamingoModel(config)
 
-# # add <answer> token to tokenizer
-# model.text_tokenizer.add_special_tokens(
-#     {"additional_special_tokens": ["<|endofchunk|>", "<image>", "<answer>"]}
-# )
+# # # add <answer> token to tokenizer
+# # model.text_tokenizer.add_special_tokens(
+# #     {"additional_special_tokens": ["<|endofchunk|>", "<image>", "<answer>"]}
+# # )
 
-# model.lang_encoder.resize_token_embeddings(len(model.text_tokenizer))
-# model.load_state_dict(torch.load("/home/v-boli7/azure_storage/models/multi_instruct_conversation-58k-nocontext_flamingo9B_lr1e-5/final_weight_hf.pt", map_location="cpu"), strict=False)
+# # model.lang_encoder.resize_token_embeddings(len(model.text_tokenizer))
+# model.load_state_dict(torch.load("/home/v-boli7/azure_storage/models/openflamingo/open_flamingo_hf.pt", map_location="cpu"), strict=False)
 # model.save_pretrained("./flamingo_9b_hf")
 
-from flamingo_hf import FlamingoModel
+from flamingo_hf import FlamingoModel, FlamingoForConditionalGeneration
 
-model = FlamingoModel.from_pretrained(
+model = FlamingoForConditionalGeneration.from_pretrained(
     "flamingo_9b_hf",
     device_map="auto",
     load_in_8bit=False,
@@ -38,7 +38,6 @@ demo_image_two = Image.open(requests.get("http://images.cocodataset.org/test-stu
 query_image = Image.open(requests.get("http://images.cocodataset.org/test-stuff2017/000000028352.jpg", stream=True).raw)
 
 vision_x = image_processor.preprocess([demo_image_one, demo_image_two, query_image], return_tensors="pt")["pixel_values"].unsqueeze(1).unsqueeze(0)
-vision_x = vision_x.to(model.device)
 """
 Step 3: Preprocessing text
 Details: In the text we expect an <image> special token to indicate where an image is.
@@ -55,10 +54,10 @@ lang_x = model.text_tokenizer(
 Step 4: Generate text
 """
 generated_text = model.generate(
-    vision_x=vision_x,
-    lang_x=lang_x["input_ids"],
-    attention_mask=lang_x["attention_mask"],
-    max_new_tokens=20,
+    vision_x=vision_x.to(model.device),
+    lang_x=lang_x["input_ids"].to(model.device),
+    attention_mask=lang_x["attention_mask"].to(model.device),
+    max_new_tokens=50,
     num_beams=3,
 )
 
