615ce2ad1a1e90881be8c1b75ba93ef4b49f4254
Bo Li <drluodian@gmail.com>
2023-04-29 11:55:57 +0000

    update README.md
    Former-commit-id: 5247c165f639908f6255b9306c446bdc63a7c51f

--- otter/otter_pt2otter_hf.py
+++ otter/otter_pt2otter_hf.py
@@ -108,7 +108,7 @@ def dump_hf_model(old_ckpt_path: str, new_folder_path: str) -> None:
     if old_ckpt.get("model", None) is not None:
         old_ckpt = old_ckpt["model"]
     new_ckpt = rename_old_checkpoint(old_ckpt)
-    config = OtterConfig.from_json_file("otter_hf/config.json")
+    config = OtterConfig.from_json_file("otter/config.json")
     model = OtterModel(config)
     model.load_state_dict(new_ckpt, strict=False)
     print(f"Saving HF model to {new_folder_path}")
