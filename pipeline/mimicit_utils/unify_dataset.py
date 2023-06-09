d45aaae5ebf06d652f713f7518c632915da39d5f
Bo Li <drluodian@gmail.com>
2023-06-09 03:38:43 +0000

    update minor copyright issues

--- pipeline/mimicit_utils/unify_dataset.py
+++ pipeline/mimicit_utils/unify_dataset.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The Otter Team.
+# Copyright 2023 The Otter Team.
 # All rights reserved.
 # This source code is licensed under the Apache 2.0 license
 # found in the LICENSE file in the root directory.
