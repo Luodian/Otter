cf83a03cfe05a2b081b5ccfb7b457d299f9a62c8
Li Bo <drluodian@gmail.com>
2023-06-23 03:06:03 +1000

    add convert_mmc4 and changes in gradio demos (#163)

--- pipeline/utils/convert_mmc4_to_wds.py
+++ pipeline/utils/convert_mmc4_to_wds.py
@@ -4,8 +4,7 @@ import json
 import os
 import tarfile
 import uuid
-import zipfile
-
+import sys
 import braceexpand
 import webdataset as wds
 
@@ -19,53 +18,55 @@ arg_parser.add_argument(
 arg_parser.add_argument(
     "--doc_shards",
     type=str,
-    help="Pass in a list of shards in the format path_to_shard/docs_shard_{0..23098}_v2.jsonl.zip",
+    help="Pass in a list of shards in the format path_to_shard/docs_shard_{0..23098}_v2.jsonl",
 )
 args = arg_parser.parse_args()
 
+from tqdm import tqdm
 
-def main():
+
+def main(args, start_number=0):
     os.makedirs(args.output_dir, exist_ok=True)
 
     doc_shards = list(braceexpand.braceexpand(args.doc_shards))
     image_shards = list(braceexpand.braceexpand(args.image_shards))
 
-    assert len(doc_shards) == len(image_shards), "Each doc shards must have a corresponding image shard"
+    assert len(doc_shards) == len(image_shards), "Each doc shard must have a corresponding image shard"
+    with wds.ShardWriter(args.output_dir + f"/%09d.tar", maxcount=30000, maxsize=1e10) as sink:
+        for idx in tqdm(range(start_number, len(doc_shards)), desc="Converting shards"):
+            try:
+                image_tar = tarfile.open(image_shards[idx])
+            except Exception as e:
+                print(e)
+                continue
 
-    with wds.ShardWriter(args.output_dir + "/%09d.tar", maxcount=1000) as sink:
-        for idx in range(len(doc_shards)):
-            image_tar = tarfile.open(image_shards[idx])
-
-            # Open the ZIP archive and extract the JSON file
-            with zipfile.ZipFile(doc_shards[idx], "r") as zip_file:
-                # Assumes the JSON file is the first file in the archive
-                json_filename = zip_file.namelist()[0]
-                with zip_file.open(json_filename, "r") as json_file:
+            # Read the JSONL file
+            try:
+                with open(doc_shards[idx], "r") as json_file:
                     for sample_data in json_file:
                         # get image names from json
                         sample_data = json.loads(sample_data)
                         image_info = sample_data["image_info"]
                         image_names = [image["image_name"] for image in image_info]
 
-                        found_key = True
                         # Add each image to the tar file
                         for img_idx, image_name in enumerate(image_names):
-                            try:
-                                image = image_tar.extractfile(f"{image_tar.getnames()[0]}/{image_name}")
-                                # convert to base64
-                                image_bytes = image.read()
-                                image_base64 = base64.b64encode(image_bytes).decode("utf-8")
-                                sample_data["image_info"][img_idx]["image_base64"] = image_base64
-                            except Exception as e:
-                                print(f"Error: {e}")
-                                found_key = False
+                            image = image_tar.extractfile(f"{image_tar.getnames()[0]}/{image_name}")
 
-                        if found_key:
-                            key_str = uuid.uuid4().hex
-                            sink.write({"__key__": key_str, "json": sample_data})
+                            # convert to base64
+                            image_bytes = image.read()
+                            image_base64 = base64.b64encode(image_bytes).decode("utf-8")
+                            sample_data["image_info"][img_idx]["image_base64"] = image_base64
+
+                        key_str = uuid.uuid4().hex
+                        sink.write({"__key__": key_str, "json": sample_data})
+            except Exception as e:
+                print(e)
+                image_tar.close()
+                continue
 
             image_tar.close()
 
 
 if __name__ == "__main__":
-    main()
+    main(args=args)
