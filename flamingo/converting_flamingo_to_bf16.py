d9c4ef8cb72e8ad7badf2ff3f0f7d0e4146da088
Li Bo <drluodian@gmail.com>
2023-06-25 18:09:50 +1000

    [Feat] update code for converting to other precision (#171)
    * refactor code and succesfully run on 4GPUs
    
    * Squashed commit of the following:
    
    commit 885e70939c3cf90a11802b4391152df9c1426ed6
    Merge: b28fe59 ea224f2
    Author: ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
    Date:   Sat Jun 24 17:25:13 2023 +0800
    
        Merge branch 'yhzhang/dev_mmc4' of https://github.com/Luodian/Otter into yhzhang/dev_mmc4
    
    commit b28fe59f144423868fe057b1a5fd9e6237079758
    Author: ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
    Date:   Sat Jun 24 14:44:41 2023 +0800
    
        update
    
    * update flamingo-mpt-30B
    
    * update injecting and converting to bf16 codes
    
    * update converting code
    
    * Squashed commit of the following:
    
    commit 2eaa77124a4faab241f51f25fb796ec4e87ad9d4
    Author: ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
    Date:   Sun Jun 25 11:48:25 2023 +0800
    
        add falcon-7b
    
    commit 7ecc37ee972f19bb29b9ac29bdb7574db9cda49f
    Author: Li Bo <drluodian@gmail.com>
    Date:   Sun Jun 25 12:53:41 2023 +1000
    
        [Feat/Model] development for MPT-30B model on MMC4 (#168)
    
        * refactor code and succesfully run on 4GPUs
    
        * Squashed commit of the following:
    
        commit 885e70939c3cf90a11802b4391152df9c1426ed6
        Merge: b28fe59 ea224f2
        Author: ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
        Date:   Sat Jun 24 17:25:13 2023 +0800
    
            Merge branch 'yhzhang/dev_mmc4' of https://github.com/Luodian/Otter into yhzhang/dev_mmc4
    
        commit b28fe59f144423868fe057b1a5fd9e6237079758
        Author: ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
        Date:   Sat Jun 24 14:44:41 2023 +0800
    
            update
    
        * update flamingo-mpt-30B
    
    * remove unused data preprocessing scripts
    
    * update configs to avoid load to wrong devices
    
    * update code for converting to other precision

--- flamingo/converting_flamingo_to_bf16.py
+++ flamingo/converting_flamingo_to_bf16.py
@@ -8,9 +8,11 @@ from .modeling_flamingo import FlamingoForConditionalGeneration
 
 parser = argparse.ArgumentParser(description="Load model with precision")
 parser.add_argument("--load_bit", type=str, choices=["fp16", "bf16"], required=True, help="Choose either 'fp16' or 'bf16'")
+parser.add_argument("--pretrained_model_path", type=str, default="/home/luodian/projects/checkpoints/flamingo-mpt-7B-instruct-init", required=True)
 args = parser.parse_args()
 
 load_bit = args.load_bit
+pretrained_model_path = args.pretrained_model_path
 
 if load_bit == "fp16":
     precision = {"torch_dtype": torch.float16}
@@ -19,10 +21,9 @@ elif load_bit == "bf16":
 
 root_dir = os.environ["AZP"]
 print(root_dir)
-checkpoint_path = f"{root_dir}/otter/checkpoints/flamingo-mpt-7B-instruct-init"
 device_id = "cpu"
-model = FlamingoForConditionalGeneration.from_pretrained(checkpoint_path, device_map={"": device_id}, **precision)
+model = FlamingoForConditionalGeneration.from_pretrained(pretrained_model_path, device_map={"": device_id}, **precision)
 
-# save model
-checkpoint_path = checkpoint_path + f"_{load_bit}"
+# save model to same folder
+checkpoint_path = pretrained_model_path + f"-{load_bit}"
 model.save_pretrained(checkpoint_path)
