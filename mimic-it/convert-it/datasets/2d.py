7ed6aff01ee18649215d1bb58075c347cea6ea3e
Bo Li <drluodian@gmail.com>
2023-07-02 04:01:47 +0000

    merge from main's changes

--- mimic-it/convert-it/datasets/2d.py
+++ mimic-it/convert-it/datasets/2d.py
@@ -1,11 +1,6 @@
-import os
 import json
 
 from abstract_dataset import AbstractDataset
-from PIL import Image
-from tqdm import tqdm
-from glob import glob
-from image_utils import create_folder
 
 
 class Llava(AbstractDataset):
@@ -24,27 +19,30 @@ class Llava(AbstractDataset):
         Args:
             name (str): The name of the dataset. Defaults to "Llava".
             short_name (str): The short name of the dataset. Defaults to "LA".
-            image_path (str): The path containing the dataset images, downloaded from https://drive.google.com/file/d/1OVb4_3Uec_xbyUk90aWC6LFpKsIOtR7v/view?usp=sharing.
-            image_root (str): The path to the coco image train split
+            image_root (str): The root path to the COCO image train split.
+            image_path (str): The path to the JSON file containing the dataset images.
+                              The images can be downloaded from:
+                              https://drive.google.com/file/d/1OVb4_3Uec_xbyUk90aWC6LFpKsIOtR7v/view?usp=sharing.
             num_threads (int): The number of threads to use for processing the images.
         """
         self.image_root = image_root
         super().__init__(name, short_name, image_path, num_threads)
 
-    def _load_images(self, image_path: str, num_thread: int) -> dict[str, Image.Image]:
+    def _load_images(self, image_path: str, num_thread: int) -> dict[str, bytes]:
         """
         Loads the images from the dataset.
 
         Args:
-            image_path (str): The path to the dictionary containing the dataset images.
+            image_path (str): The path to the JSON file containing the dataset images.
             num_threads (int): The number of threads to use for processing the images.
 
         Returns:
-            dict[str, Image.Image]: A dictionary where the keys are image identifiers and the values are PIL.Image.Image objects.
+            dict[str, bytes]: A dictionary where the keys are image identifiers and the values are bytes objects representing the images.
         """
 
-        def read_image(file_name) -> Image.Image:
-            return Image.open(file_name)
+        def read_image(file_name) -> bytes:
+            with open(file_name, "rb") as f:
+                return f.read()
 
         images = {}
         with open(image_path) as f:
--- mimic-it/convert-it/datasets/utils/scene_navigation_utils.py
+++ mimic-it/convert-it/datasets/utils/scene_navigation_utils.py
@@ -1,24 +1,23 @@
 import os
 
-from PIL import Image
+from glob import glob
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
-from PIL import Image
-from image_utils import resize_image
+from image_utils import process_image
 
 
 def process(cur_dir, img_root):
     """
     Process images in a directory.
 
-    Parameters:
-    - cur_dir (str): The current directory name.
-    - img_root (str): The root directory of the images.
+    Args:
+        cur_dir (str): The name of the current directory.
+        img_root (str): The root directory of the images.
 
     Returns:
-    - dict: A dictionary containing processed images. The keys are unique identifiers
-            for each image, and the values are the processed images.
+        dict: A dictionary containing processed images. The keys are unique identifiers
+        for each image, and the values are the processed images.
 
     """
     root = os.path.join(img_root, cur_dir, "color")
@@ -26,9 +25,10 @@ def process(cur_dir, img_root):
     images = {}
     for cur_file in file_list:
         file_name = os.path.join(img_root, cur_dir, "color", cur_file)
-        img = Image.open(file_name)  # path to file
+        with open(file_name, "rb") as f:
+            img = f.read()
         image_id = f"{cur_dir}_color_{cur_file[:-4]}"
-        images[image_id] = resize_image(img)
+        images[image_id] = process_image(img)
     return images
 
 
@@ -36,23 +36,22 @@ def process_data(img_root: str, num_threads: int):
     """
     Process images in parallel using multiple threads.
 
-    Parameters:
-    - img_root (str): The root directory of the images.
-    - num_threads (int): The number of threads to use for parallel processing.
+    Args:
+        img_root (str): The root directory of the images.
+        num_threads (int): The number of threads to use for parallel processing.
 
     Returns:
-    - dict: A dictionary containing processed images. The keys are unique identifiers
-            for each image, and the values are the processed images.
+        dict: A dictionary containing processed images. The keys are unique identifiers
+        for each image, and the values are the processed images.
 
     """
-    keys = os.listdir(img_root)
+    keys_dir = glob(os.path.join(img_root, "scene*_00"))
+    keys = list(map(os.path.basename, keys_dir))
     all_images = {}
     process_bar = tqdm(total=len(keys), unit="image", desc="Loading images")
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        futures = [executor.submit(process, cur_dir, img_root) for cur_dir in keys]
-        for future in futures:
-            images = future.result()
+        for images in executor.map(process, keys, [img_root] * len(keys)):
             all_images.update(images)
-            process_bar.update(1)
+            process_bar.update()
     process_bar.close()
     return all_images
