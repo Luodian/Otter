a13a2c453032e2d09798ad06a0a760041602c71d
ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
2023-07-04 16:13:39 +0800

    update

--- otter/modeling_otter.py
+++ otter/modeling_otter.py
@@ -768,6 +768,7 @@ class OtterForConditionalGeneration(OtterPreTrainedModel):
         )
         self.post_init()
 
+        
     def get_input_embeddings(self) -> nn.Module:
         return self.lang_encoder.get_input_embeddings()
 
--- pipeline/train/instruction_following.py
+++ pipeline/train/instruction_following.py
@@ -28,11 +28,6 @@ from pipeline.train.train_utils import AverageMeter, get_checkpoint
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-<<<<<<< HEAD
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-=======
->>>>>>> update
 # The flag below controls whether to allow TF32 on matmul. This flag defaults to False
 # in PyTorch 1.12 and later.
 torch.backends.cuda.matmul.allow_tf32 = True
@@ -392,7 +387,7 @@ def main():
             )
 
     accelerator.wait_for_everyone()
-
+    
     if model.lang_encoder.__class__.__name__ != "MPTForCausalLM":
         model.lang_encoder.resize_token_embeddings(len(model.text_tokenizer))
 
