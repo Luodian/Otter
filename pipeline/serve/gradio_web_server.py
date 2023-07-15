448ae8c413110cc49fe033fd2064fced726ddf41
Li Bo <drluodian@gmail.com>
2023-07-15 21:03:49 +0800

    check if git account configured back to Li Bo

--- pipeline/serve/gradio_web_server.py
+++ pipeline/serve/gradio_web_server.py
@@ -612,7 +612,7 @@ def build_demo(embed_mode):
                 [f"{cur_dir}/examples/ms_st.jpg", "Does the image feature a globally recognized technology company?"],
                 [f"{cur_dir}/examples/ms_st.jpg", "Does the image feature a globally recognized technology company? Please answer with yes or no."],
                 [f"{cur_dir}/examples/zelda_princess.jpg", "Can you identify the game character?"],
-                [f"{cur_dir}/examples/martin.jpeg", "Can you identify the historical figure?"],
+                [f"{cur_dir}/examples/martin.jpeg", "Can you identify the historic figure?"],
                 [f"{cur_dir}/examples/gtav.jpg", "Can you identify what the image is about?"],
                 [
                     f"{cur_dir}/examples/xray.jpg",
