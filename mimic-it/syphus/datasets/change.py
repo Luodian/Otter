824e00bcd3172c77449a4ab04f83bbbecaa8fab6
Li Bo <LIBO0013@e.ntu.edu.sg>
2023-08-02 18:23:06 +1000

    [Datasets] Add CGD Prompts and Query code (#231)
    * add cgd in syphus
    
    * black lint

--- mimic-it/syphus/datasets/change.py
+++ mimic-it/syphus/datasets/change.py
@@ -2,6 +2,7 @@
 This file contains the implementation of the SpotTheDifference and CleverChange datasets.
 """
 
+import importlib
 import json
 
 from abstract_dataset import AbstractDataset
@@ -30,3 +31,34 @@ class SpotTheDifference(AbstractDataset):
                 }
             )
         return results
+
+
+class CocoSpotTheDifference(AbstractDataset):
+    def __init__(
+        self,
+        name: str = "CocoSpotTheDifference",
+        prompt_path: str = "prompts.coco_spot_the_difference_prompt",
+        query_inputs_path: str = "annotations/coco_spot_the_difference/csd_query.json",
+    ):
+        super().__init__(name, prompt_path, query_inputs_path)
+
+    def _load_query_inputs(self, path: str) -> list[dict[str, str]]:
+        with open(path) as f:
+            json_data = json.load(f)
+        results = []
+        for file_id, inner_dict in json_data.items():
+            sentences = inner_dict["sentences"]
+            results.append(
+                {
+                    "id": file_id,
+                    "sentences": sentences,
+                }
+            )
+        return results
+
+    def _load_prompt(self, path: str) -> dict[str, str]:
+        prompt_file = importlib.import_module(path)
+        return {
+            "system_message": prompt_file.system_message,
+            "in_context": prompt_file.in_context,
+        }
