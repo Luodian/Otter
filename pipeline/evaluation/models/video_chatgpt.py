2e23aecfae67aafcea5c8a4d01fa75e387bd0a23
ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
2023-09-14 11:28:29 +0800

    add video chatGPT and owl

--- pipeline/evaluation/models/video_chatgpt.py
+++ pipeline/evaluation/models/video_chatgpt.py
@@ -1,32 +1,28 @@
-
 import torch
-from video_chatgpt.eval.model_utils import load_video, initialize_model
-from video_chatgpt.inference import video_chatgpt_infer
+from .video_chatgpt.eval.model_utils import load_video, initialize_model
+from .video_chatgpt.inference import video_chatgpt_infer
 
 from .base_model import BaseModel
 
 model_name = "/mnt/lustre/yhzhang/kaichen/video_ChatGPT/LLaVA-Lightening-7B-v1-1."
 projection_path = "/mnt/lustre/yhzhang/kaichen/video_ChatGPT/video_chatgpt-7B.bin"
 
+
 class Video_ChatGPT(BaseModel):
     def __init__(self, model_path: str):
         super().__init__(model_path)
-        self.model, self.vision_tower, self.tokenizer, self.image_processor, self.video_token_len = \
-        initialize_model(model_name, projection_path)
-    
+        self.model, self.vision_tower, self.tokenizer, self.image_processor, self.video_token_len = initialize_model(model_name, projection_path)
+
     def generate(self, input_data: dict):
-        video_frames = load_video(input_data["video_idx"])
-        question = input_data['question']
-        output = video_chatgpt_infer(video_frames, 
-                                     question, 
-                                     conv_mode='video-chatgpt_v1', 
-                                     model=self.model, 
-                                     vision_tower=self.vision_tower,
-                                     tokenizer=self.tokenizer, 
-                                     image_processor=self.image_processor, 
-                                     video_token_len=self.video_token_len)
+        video_dir = input_data.get("video_root", "")
+        video_frames = load_video(os.path.join(video_dir, input_data["video_idx"] + ".mp4"))
+        question = input_data["question"]
+        output = video_chatgpt_infer(
+            video_frames, question, conv_mode="video-chatgpt_v1", model=self.model, vision_tower=self.vision_tower, tokenizer=self.tokenizer, image_processor=self.image_processor, video_token_len=self.video_token_len
+        )
         return output
-    
+
+
 if __name__ == "__main__":
     model = Video_ChatGPT("")
     device = torch.device("cuda")
