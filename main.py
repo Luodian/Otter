577499a72a6dc055918da59ba49b148b2ade190c
kassy11 <kotaro11kassy@gmail.com>
2023-09-21 17:34:08 +0900

    delete otter_video main function

--- main.py
+++ main.py
@@ -15,38 +15,60 @@ from pipeline.demo.otter_video import get_response, get_image
 
 requests.packages.urllib3.disable_warnings()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_video_path", type=str, required=True, help="path for input mp4 video")
-args = parser.parse_args()
 
-# TODO: loggingを導入する
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input_video_path", type=str, required=True, help="path for input mp4 video"
+    )
+    args = parser.parse_args()
 
-# model settings
-load_bit = "fp32"
-if load_bit == "fp16":
-    precision = {"torch_dtype": torch.float16}
-elif load_bit == "bf16":
-    precision = {"torch_dtype": torch.bfloat16}
-elif load_bit == "fp32":
-    precision = {"torch_dtype": torch.float32}
+    # TODO: loggingを導入する
 
-model = OtterForConditionalGeneration.from_pretrained("luodian/OTTER-9B-DenseCaption", device_map="auto", **precision)
-tensor_dtype = {"fp16": torch.float16, "bf16": torch.bfloat16, "fp32": torch.float32}[load_bit]
+    # model settings
+    load_bit = "fp32"
+    if load_bit == "fp16":
+        precision = {"torch_dtype": torch.float16}
+    elif load_bit == "bf16":
+        precision = {"torch_dtype": torch.bfloat16}
+    elif load_bit == "fp32":
+        precision = {"torch_dtype": torch.float32}
 
-model.text_tokenizer.padding_side = "left"
-tokenizer = model.text_tokenizer
-image_processor = transformers.CLIPImageProcessor()
-model.eval()
+    model = OtterForConditionalGeneration.from_pretrained(
+        "luodian/OTTER-9B-DenseCaption", device_map="auto", **precision
+    )
+    tensor_dtype = {
+        "fp16": torch.float16,
+        "bf16": torch.bfloat16,
+        "fp32": torch.float32,
+    }[load_bit]
 
-# read mp4
-video_url = args.input_video_path
-print(video_url)
-frames_list = get_image(video_url)
+    model.text_tokenizer.padding_side = "left"
+    tokenizer = model.text_tokenizer
+    image_processor = transformers.CLIPImageProcessor()
+    model.eval()
 
-# TODO: プロンプトのファイル数だけ繰り返す
-# otter model input and output
-prompts_input = input("Enter prompts: ")
-response = get_response(frames_list, prompts_input, model, image_processor, tensor_dtype)
-print(f"Response: {response}")
+    # read mp4
+    video_url = args.input_video_path
 
-# TODO: 回答をCSVに書き込む
+    if video_url[-3] != "MP4" or video_url[-3] != "mp4":
+        # TODO: logging
+        print("mp4ファイルを指定してください。")
+        return
+
+    print(video_url)
+    frames_list = get_image(video_url)
+
+    # TODO: プロンプトのファイル数だけ繰り返す
+    # otter model input and output
+    prompts_input = input("Enter prompts: ")
+    response = get_response(
+        frames_list, prompts_input, model, image_processor, tensor_dtype
+    )
+    print(f"Response: {response}")
+
+    # TODO: 回答をCSVに書き込む
+
+
+if __name__ == "__main__":
+    main()
--- pipeline/demo/otter_video.py
+++ pipeline/demo/otter_video.py
@@ -135,37 +135,3 @@ def get_response(input_data, prompt: str, model=None, image_processor=None, tens
         .rstrip('"')
     )
     return parsed_output
-
-
-# ------------------- Main Function -------------------
-load_bit = "fp32"
-if load_bit == "fp16":
-    precision = {"torch_dtype": torch.float16}
-elif load_bit == "bf16":
-    precision = {"torch_dtype": torch.bfloat16}
-elif load_bit == "fp32":
-    precision = {"torch_dtype": torch.float32}
-
-# This model version is trained on MIMIC-IT DC dataset.
-model = OtterForConditionalGeneration.from_pretrained("luodian/OTTER-9B-DenseCaption", device_map="auto", **precision)
-tensor_dtype = {"fp16": torch.float16, "bf16": torch.bfloat16, "fp32": torch.float32}[load_bit]
-
-model.text_tokenizer.padding_side = "left"
-tokenizer = model.text_tokenizer
-image_processor = transformers.CLIPImageProcessor()
-model.eval()
-
-while True:
-    video_url = input("Enter video path: ")  # Replace with the path to your video file, could be any common format.
-
-    frames_list = get_image(video_url)
-
-    while True:
-        prompts_input = input("Enter prompts: ")
-
-        if prompts_input.lower() == "quit":
-            break
-
-        print(f"\nPrompt: {prompts_input}")
-        response = get_response(frames_list, prompts_input, model, image_processor, tensor_dtype)
-        print(f"Response: {response}")
