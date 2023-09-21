49c62e8712eff956144cb42a4464b5ba8c587f84
kassy11 <kotaro11kassy@gmail.com>
2023-09-21 23:14:57 +0900

    read prompt from txt files

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import transformers
 from PIL import Image
 import sys
 import logging
+import glob
+import utils
 
 sys.path.append("./src")
 from src.otter_ai import OtterForConditionalGeneration
@@ -19,11 +21,16 @@ requests.packages.urllib3.disable_warnings()
 logger = logging.getLogger("alcon_log")
 logger.setLevel(logging.INFO)
 
+PROMPT_PATH = "./prompt"
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--input_video_path", type=str, required=True, help="path for input mp4 video."
+    )
+    parser.add_argument(
+        "--output_csv_path", type=str, default="output.csv", help="path for output csv."
     )
     parser.add_argument(
         "--is_transparent_background",
@@ -67,23 +74,25 @@ def main():
     video_url = args.input_video_path
 
     if video_url[-3:] != "MP4" and video_url[-3:] != "mp4":
-        # TODO: logging
         logger.error("mp4ファイルを指定してください。")
         return
 
     frames_list = get_image(video_url, args.is_transparent_background)
 
     # TODO: プロンプトのファイル数だけ繰り返す
-    # otter model input and output
-    prompts_input = input("Enter prompts: ")
-    logger.info("Generating answer...")
-    response = get_response(
-        frames_list, prompts_input, model, image_processor, tensor_dtype
-    )
-    logger.info(f"Response: {response}")
-    print(f"Response: {response}")
-
-    # TODO: 回答をCSVに書き込む
+    prompt_files = glob.glob(os.path.join(PROMPT_PATH, "*.txt"))
+    print(prompt_files)
+    for prompt_file in prompt_files:
+        with open(prompt_file, "r") as file:
+            prompt = file.read()
+            # otter model input and output
+            logger.info("Generating answer...")
+            response = get_response(
+                frames_list, prompt, model, image_processor, tensor_dtype
+            )
+            fish_num = utils.extract_number_from_response(response)
+            # TODO: CSVに書き込む
+            logger.info(f"Response: {response}")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,53 +1,17 @@
 import cv2
 import numpy as np
+import re
+from word2number import w2n
 
 
-# 出力csvと正解csvを比較して、検出結果の精度を出す
+# TODO: 出力csvと正解csvを比較して、検出結果の精度を出す
 def accuracy():
     return
 
 
-# # TODO：出力ファイルの指定をなくす
-# def remove_background(input_video_path):
-#     output_video_path = "output_temp.MP4"
-#     cap = cv2.VideoCapture(input_video_path)
-
-#     # 動画のフレームサイズを取得
-#     # TODO: マジックナンバーを消す
-#     frame_width = int(cap.get(3))
-#     frame_height = int(cap.get(4))
-
-#     video_frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-#     video_fps = cap.get(cv2.CAP_PROP_FPS)
-#     video_len_sec = video_frame_count / video_fps
-
-#     # 出力動画ファイルの設定
-#     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-#     out = cv2.VideoWriter(output_video_path, fourcc, 30, (frame_width, frame_height), isColor=True)
-
-#     # 背景差分法の初期化
-#     fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
-
-#     while True:
-#         ret, frame = cap.read()
-#         if not ret:
-#             break
-
-#         # 背景差分法を適用して前景を取得
-#         fg_mask = fgbg.apply(frame)
-
-#         # 前景と背景の差分を反転させて背景を取得
-#         bg_mask = cv2.bitwise_not(fg_mask)
-
-#         # 背景をマスクする
-#         result = cv2.bitwise_and(frame, frame, mask=fg_mask)
-
-#         # 出力動画にフレームを書き込み
-#         out.write(result)
-
-#         if cv2.waitKey(int(video_len_sec)):
-#             break
-
-#     # リソースを解放
-#     cap.release()
-#     out.release()
+def extract_number_from_response(response):
+    try:
+        result = w2n.word_to_num(response)
+    except:
+        result = 0
+    return result
