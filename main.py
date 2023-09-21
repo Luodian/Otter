2a985d17689a7620d4a8b2a33808e9fabb43b051
kassy11 <kotaro11kassy@gmail.com>
2023-09-21 15:06:02 +0900

    add --input_video_path arg

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import mimetypes
 import os
 from typing import Union
@@ -9,17 +10,15 @@ from PIL import Image
 import sys
 
 sys.path.append("./src")
-# make sure you can properly access the otter folder
 from src.otter_ai import OtterForConditionalGeneration
 from pipeline.demo.otter_video import get_response, get_image
 
-# from Otter.pipeline.demo
-
-# Disable warnings
 requests.packages.urllib3.disable_warnings()
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_video_path", type=str, required=True, help="path for input mp4 video")
+args = parser.parse_args()
 
-# ------------------- Main Function -------------------
 load_bit = "fp32"
 if load_bit == "fp16":
     precision = {"torch_dtype": torch.float16}
@@ -28,7 +27,6 @@ elif load_bit == "bf16":
 elif load_bit == "fp32":
     precision = {"torch_dtype": torch.float32}
 
-# This model version is trained on MIMIC-IT DC dataset.
 model = OtterForConditionalGeneration.from_pretrained("luodian/OTTER-9B-DenseCaption", device_map="auto", **precision)
 tensor_dtype = {"fp16": torch.float16, "bf16": torch.bfloat16, "fp32": torch.float32}[load_bit]
 
@@ -37,17 +35,13 @@ tokenizer = model.text_tokenizer
 image_processor = transformers.CLIPImageProcessor()
 model.eval()
 
-while True:
-    video_url = input("Enter video path: ")  # Replace with the path to your video file, could be any common format.
+video_url = args.input_video_path
 
-    frames_list = get_image(video_url)
+frames_list = get_image(video_url)
 
-    while True:
-        prompts_input = input("Enter prompts: ")
+# TODO: プロンプトのファイル数だけ繰り返す
+prompts_input = input("Enter prompts: ")
+response = get_response(frames_list, prompts_input, model, image_processor, tensor_dtype)
+print(f"Response: {response}")
 
-        if prompts_input.lower() == "quit":
-            break
-
-        print(f"\nPrompt: {prompts_input}")
-        response = get_response(frames_list, prompts_input, model, image_processor, tensor_dtype)
-        print(f"Response: {response}")
+# TODO: 回答をCSVに書き込む
