a8f66aa5cf476dcaae08d745b4bfd32654826678
kassy11 <kotaro11kassy@gmail.com>
2023-09-22 11:17:36 +0900

    reuslt to output csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import mimetypes
 import os
 from typing import Union
@@ -25,12 +26,21 @@ PROMPT_PATH = "./prompt"
 
 
 def main():
+    # command line args
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--input_video_path", type=str, required=True, help="path for input mp4 video."
     )
     parser.add_argument(
-        "--output_csv_path", type=str, default="output.csv", help="path for output csv."
+        "--output_csv_path",
+        type=str,
+        default="./output.csv",
+        help="path for output csv.",
+    )
+    parser.add_argument(
+        "--truth_csv_path",
+        type=str,
+        help="path for ground truth csv of output csv. if set calculate accuracy.",
     )
     parser.add_argument(
         "--is_transparent_background",
@@ -72,27 +82,45 @@ def main():
 
     # read mp4
     video_url = args.input_video_path
-
     if video_url[-3:] != "MP4" and video_url[-3:] != "mp4":
         logger.error("mp4ファイルを指定してください。")
         return
-
     frames_list = get_image(video_url, args.is_transparent_background)
 
-    # TODO: プロンプトのファイル数だけ繰り返す
+    # read prompt from ./prompt/*.txt
+    result = {}
     prompt_files = glob.glob(os.path.join(PROMPT_PATH, "*.txt"))
     print(prompt_files)
     for prompt_file in prompt_files:
-        with open(prompt_file, "r") as file:
+        fish_name = os.path.splitext(os.path.basename(prompt_file))[0]
+        with open(prompt_file, "r", encoding="utf-8") as file:
             prompt = file.read()
+            logger.info(f"Prompt: {prompt}")
             # otter model input and output
             logger.info("Generating answer...")
             response = get_response(
                 frames_list, prompt, model, image_processor, tensor_dtype
             )
-            fish_num = utils.extract_number_from_response(response)
-            # TODO: CSVに書き込む
             logger.info(f"Response: {response}")
+            # extract fish number
+            fish_num = utils.extract_number_from_response(response)
+            result[fish_name] = fish_num
+    logger.info(f"Result: {result}")
+
+    result = utils.convert_fish_name_to_number(result)
+    logger.info(f"Converted Result: {result}")
+    # calculate accuracy
+    if args.truth_csv_path:
+        # TODO: CSVからdictを読み取る
+        with open(args.truth_csv_path, "r", encoding="utf-8") as file:
+            # TODO: ここ３行目から読み取れてる？
+            reader = csv.reader(file)[2:]
+            ground_truth = {rows[0]: rows[1] for rows in reader}
+        accuracy = utils.accuracy(result, ground_truth)
+        logger.info(f"Accuracy: {accuracy}")
+
+    # output
+    utils.output_csv(result, video_url, args.output_csv_path)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,12 +1,55 @@
+import csv
+from enum import IntEnum
 import cv2
 import numpy as np
 import re
 from word2number import w2n
 
 
-# TODO: 出力csvと正解csvを比較して、検出結果の精度を出す
-def accuracy():
+FISH_TYPES = 5
+
+
+class FishNames(IntEnum):
+    HIMEDAKA = 1
+    NEON_TETRA = 2
+    GLASS_CATFISH = 3
+    GUPPY = 4
+    RYUKIN = 5
+
+    @staticmethod
+    def from_name(name):
+        for v in FishNames:
+            if name == v.name:
+                return int(v)
+        raise ValueError(f"{name} is not a valid FishNames!")
+
+
+def accuracy(result, ground_truth):
+    count = 0
+    for v in FishNames:
+        if result[int(v)] == ground_truth[int(v)]:
+            count += 1
+    accuracy = 100 * (count / FISH_TYPES)
+    return accuracy
+
+
+def output_csv(result, input_video_path, output_csv_path):
+    with open(output_csv_path, "w", encoding="utf-8") as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow([input_video_path])
+        csv_writer.writerow([FISH_TYPES])
+        for key in sorted(result.keys()):
+            value = result[key]
+            csv_writer.writerow([key, value])
     return
+
+
+def convert_fish_name_to_number(result):
+    converted_result = {}
+    for k, v in result.items():
+        key = FishNames.from_name(k)
+        converted_result[key] = v
+    return converted_result
 
 
 def extract_number_from_response(response):
