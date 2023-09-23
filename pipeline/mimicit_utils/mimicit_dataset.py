fb7572bf782244c3cdc2c9b47607ddd64dbb3493
Bo Li <drluodian@gmail.com>
2023-09-23 23:55:54 +0800

    update

--- pipeline/mimicit_utils/mimicit_dataset.py
+++ pipeline/mimicit_utils/mimicit_dataset.py
@@ -275,7 +275,7 @@ class MimicitDataset(Dataset):
         patch_images = torch.tensor([])
         all_texts = ""
         all_instruction_ids = in_context_example_ids + [instruction_id]
-        for idx, cur_instruction_id in enumerate(all_instruction_ids[:]):
+        for idx, cur_instruction_id in enumerate(all_instruction_ids):
             cur_instruction_image_id = self.dataset[cur_instruction_id]["image_ids"][0]
             cur_instruction = self.dataset[cur_instruction_id]["instruction"]
             cur_answer = self.dataset[cur_instruction_id]["answer"]
@@ -311,7 +311,7 @@ class MimicitDataset(Dataset):
     ):
         patch_images = torch.tensor([])
         all_texts = ""
-        all_instruction_ids = [instruction_id]
+        all_instruction_ids = in_context_example_ids + [instruction_id]
         random.shuffle(all_instruction_ids)
         for idx, cur_instruction_id in enumerate(all_instruction_ids):
             cur_instruction = self.dataset[cur_instruction_id]["instruction"]
@@ -414,7 +414,7 @@ class MimicitDataset(Dataset):
         # including multi-round conv for single image
         all_texts = ""
         all_instruction_ids = in_context_example_ids + [instruction_id]
-        for idx, cur_instruction_id in enumerate(all_instruction_ids[:]):
+        for idx, cur_instruction_id in enumerate(all_instruction_ids):
             cur_instruction = self.dataset[cur_instruction_id]["instruction"]
             cur_answer = self.dataset[cur_instruction_id]["answer"]
             cur_instruction = self.pre_question(cur_instruction)
@@ -456,7 +456,7 @@ class MimicitDataset(Dataset):
         all_texts = ""
         all_instruction_ids = in_context_example_ids + [instruction_id]
         patch_images = torch.zeros(3, 224, 224).unsqueeze(0).unsqueeze(0)
-        for idx, cur_instruction_id in enumerate(all_instruction_ids[:]):
+        for idx, cur_instruction_id in enumerate(all_instruction_ids):
             cur_instruction = self.dataset[cur_instruction_id]["instruction"]
             cur_answer = self.dataset[cur_instruction_id]["answer"]
             cur_instruction = self.pre_question(cur_instruction)
