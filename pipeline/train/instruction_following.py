6e7b0809e9a39c65a0a92ed648128fb53705d08a
Bo Li <drluodian@gmail.com>
2023-09-26 19:53:34 +0800

    add tokenizer saving behavior

--- pipeline/train/instruction_following.py
+++ pipeline/train/instruction_following.py
@@ -497,7 +497,7 @@ def main():
         accelerator.wait_for_everyone()
 
     # Save the final weights
-    save_final_weights(model, args, accelerator, processor=processor if "idefics" in args.model_name.lower() else None)
+    save_final_weights(model, args, accelerator, processor=processor if "idefics" in args.model_name.lower() else None, tokenizer=tokenizer if "llama2" in args.model_name.lower() else None)
     # accelerator.wait_for_everyone()
 
 
