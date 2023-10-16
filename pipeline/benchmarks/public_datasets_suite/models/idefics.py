6aa1863d7b6eb2c4e11f8f534f40c4c089090066
Fanyi Pu <FPU001@e.ntu.edu.sg>
2023-10-16 21:26:07 +0800

    format instructions

--- pipeline/benchmarks/public_datasets_suite/models/idefics.py
+++ pipeline/benchmarks/public_datasets_suite/models/idefics.py
@@ -1,5 +1,117 @@
 from pipeline.benchmarks.public_datasets_suite.eval_model import BaseEvalModel
+import io
+import torch
+from typing import List
+from transformers import IdeficsForVisionText2Text, AutoProcessor
+from PIL import Image
+from pipeline.train.train_utils import find_and_remove_tokens, get_image_attention_mask
+from pipeline.benchmarks.public_datasets_suite.models.utils import unwrap_model
+import base64
+import numpy as np
+import re
+
+
+def get_single_formatted_prompt(instruction: str, images: List[Image.Image]) -> List[str]:
+    end_of_utterance = "<end_of_utterance>\n"
+    image_tag = "<image>"
+    user_note = "User:"
+    assistant_note = "Assistant:"
+    change_role_tag = "<change_role>"
+    instruction = instruction.replace(f"{image_tag}{user_note}", f"{user_note}{image_tag}")
+    instruction = instruction.replace("GPT:", assistant_note)
+    instruction = instruction.replace(user_note, f"{change_role_tag}{end_of_utterance}{change_role_tag}{user_note}")
+    instruction = instruction.replace(assistant_note, f"{change_role_tag}{end_of_utterance}{change_role_tag}{assistant_note}")
+    instruction = instruction.split(image_tag)
+    assert len(instruction) == len(images) + 1, f"Number of images ({len(images)}) does not match number of image tags ({len(instruction) - 1})"
+    instruction_with_image = []
+    for i in range(len(images)):
+        instruction_with_image.append(instruction[i].strip())
+        instruction_with_image.append(images[i])
+    instruction_with_image.append(instruction[-1].strip())
+
+    final_instruction = []
+
+    def format(text):
+        if not isinstance(text, str):
+            return [text]
+        text = text.strip()
+        text = text.split(change_role_tag)
+        return text
+
+    for ins in instruction_with_image:
+        for formatted_ins in format(ins):
+            if formatted_ins is None or formatted_ins == "":
+                continue
+            if final_instruction == [] and formatted_ins == end_of_utterance:
+                continue
+            final_instruction.append(formatted_ins.strip())
+
+    if final_instruction[-1] == "Assistant:<answer>":
+        final_instruction[-1] = "Assistant:"
+    elif final_instruction[-1] != end_of_utterance:
+        final_instruction.append(end_of_utterance)
+
+    return final_instruction
+
+    # if answer == "":
+    #     return [
+    #         f"User:",
+    #         image,
+    #         question,
+    #         "<end_of_utterance>\n",
+    #         "Assistant:",
+    #     ]
+    # else:
+    #     return [
+    #         f"User:",
+    #         image,
+    #         question,
+    #         "<end_of_utterance>\n",
+    #         f"Assistant:<answer> {answer}",
+    #         "<end_of_utterance>",
+    #     ]
+
+
+def get_formatted_prompt(questions, images):
+    instructions = []
+    for ques, img in zip(questions, images):
+        instructions.append(get_single_formatted_prompt(ques, img))
+    return instructions
 
 
 class EvalModel(BaseEvalModel):
-    pass
+    def __init__(self, model_args):
+        if "model_path" in model_args:
+            model_path = model_args["model_path"]
+        else:
+            model_path = "HuggingFaceM4/idefics-9b-instruct"
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.model = IdeficsForVisionText2Text.from_pretrained(model_path, device_map={"": self.device}, torch_dtype=torch.bfloat16).to(self.device)
+        self.processor = AutoProcessor.from_pretrained(model_path)
+        if "<answer>" not in self.processor.tokenizer.special_tokens_map["additional_special_tokens"]:
+            past_special_tokens = self.processor.tokenizer.special_tokens_map["additional_special_tokens"]
+            self.processor.tokenizer.add_special_tokens({"additional_special_tokens": ["<answer>"] + past_special_tokens})
+
+        self.fake_token_image_token_id = self.processor.tokenizer("<fake_token_around_image>", add_special_tokens=False)["input_ids"][-1]
+        self.endofchunk_text = "<end_of_utterance>"
+        self.endofchunk_token_id = self.processor.tokenizer(self.endofchunk_text, add_special_tokens=False)["input_ids"][-1]
+        self.answer_token_id = self.processor.tokenizer("<answer>", add_special_tokens=False)["input_ids"][-1]
+        self.eos_token_id = self.processor.tokenizer(self.processor.tokenizer.eos_token, add_special_tokens=False)["input_ids"][-1]
+        self.patch_resize_transform = self.processor.image_processor.preprocess
+
+    def get_outputs(
+        self,
+        batch_text: List[str],
+        batch_images: List[List[Image.Image]],
+        min_generation_length: int,
+        max_generation_length: int,
+        num_beams: int,
+        length_penalty: float,
+    ) -> List[str]:
+        batch_images = list(map(lambda x: x[0], batch_images))
+        formatted_prompt = get_formatted_prompt(batch_text, batch_images)
+
+
+if __name__ == "__main__":
+    s = "<image>User: what is the brand on the bottle? Please answer in short words. GPT:<answer>"
+    print(get_single_formatted_prompt(s, ["An image"]))
