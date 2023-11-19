ce1ce35188f6f09ea7de261e67213dc7c7b199ae
Fanyi Pu <FPU001@e.ntu.edu.sg>
2023-11-19 19:49:03 +0800

    upgrade otter-ai pypi package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="otter-ai",
-    version="0.0.0-alpha-4",
+    version="0.0.0-alpha-6",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=requirements,
