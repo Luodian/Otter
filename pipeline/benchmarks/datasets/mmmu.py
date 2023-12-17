207bb00797292505447bcd1e2545d8c093757507
Bo Li <drluodian@gmail.com>
2023-12-17 10:04:35 +0800

    Fix bug in login functionality

--- pipeline/benchmarks/datasets/mmmu.py
+++ pipeline/benchmarks/datasets/mmmu.py
@@ -1,7 +1,7 @@
 from ast import Dict
 import json
 import os
-from datasets import load_dataset,  get_dataset_config_names
+from datasets import load_dataset, get_dataset_config_names
 from typing import Union
 from .base_eval_dataset import BaseEvalDataset
 from tqdm import tqdm
@@ -11,22 +11,28 @@ import random
 import numpy as np
 import re
 import ast
+
 # Many functions taken from https://github.com/MMMU-Benchmark/MMMU/ to follow the eval setup
 DOMAIN_CAT2SUB_CAT = {
-  'Art and Design': ['Art', 'Art_Theory', 'Design', 'Music'],
-  'Business': ['Accounting', 'Economics', 'Finance', 'Manage','Marketing'],
-  'Science': ['Biology', 'Chemistry', 'Geography', 'Math', 'Physics',],
-  'Health and Medicine': ['Basic_Medical_Science', 'Clinical_Medicine', 'Diagnostics_and_Laboratory_Medicine', 'Pharmacy', 'Public_Health'],
-  'Humanities and Social Science': ['History', 'Literature', 'Sociology', 'Psychology'],
-  'Tech and Engineering': ['Agriculture', 'Architecture_and_Engineering', 'Computer_Science', 'Electronics', 'Energy_and_Power', 'Materials', 'Mechanical_Engineering'],
+    "Art and Design": ["Art", "Art_Theory", "Design", "Music"],
+    "Business": ["Accounting", "Economics", "Finance", "Manage", "Marketing"],
+    "Science": [
+        "Biology",
+        "Chemistry",
+        "Geography",
+        "Math",
+        "Physics",
+    ],
+    "Health and Medicine": ["Basic_Medical_Science", "Clinical_Medicine", "Diagnostics_and_Laboratory_Medicine", "Pharmacy", "Public_Health"],
+    "Humanities and Social Science": ["History", "Literature", "Sociology", "Psychology"],
+    "Tech and Engineering": ["Agriculture", "Architecture_and_Engineering", "Computer_Science", "Electronics", "Energy_and_Power", "Materials", "Mechanical_Engineering"],
 }
-
-
 
 
 utc_plus_8 = pytz.timezone("Asia/Singapore")  # You can also use 'Asia/Shanghai', 'Asia/Taipei', etc.
 utc_now = pytz.utc.localize(datetime.datetime.utcnow())
 utc_plus_8_time = utc_now.astimezone(utc_plus_8)
+
 
 def eval_multi_choice(gold_i, pred_i):
     """
@@ -40,10 +46,11 @@ def eval_multi_choice(gold_i, pred_i):
             if answer == pred_i:
                 correct = True
                 break
-    else: # gold_i is a string
+    else:  # gold_i is a string
         if gold_i == pred_i:
             correct = True
     return correct
+
 
 def eval_open(gold_i, pred_i):
     """
@@ -58,20 +65,21 @@ def eval_open(gold_i, pred_i):
             norm_answers.extend(normalize_str(answer))
     else:
         norm_answers = normalize_str(gold_i)
-    for pred in pred_i: # pred is already normalized in parse response phase
-        if isinstance(pred, str): # if it's a string, then find if ans in the pred_i
+    for pred in pred_i:  # pred is already normalized in parse response phase
+        if isinstance(pred, str):  # if it's a string, then find if ans in the pred_i
             for norm_ans in norm_answers:
                 # only see if the string answer in the string pred
                 if isinstance(norm_ans, str) and norm_ans in pred:
                     if not correct:
                         correct = True
                     break
-        else: # it's a float number
+        else:  # it's a float number
             if pred in norm_answers:
                 if not correct:
                     correct = True
                 break
     return correct
+
 
 def evaluate_mmmu(samples):
     """
@@ -81,26 +89,29 @@ def evaluate_mmmu(samples):
     pred_correct = 0
     judge_dict = dict()
     for sample in samples:
-        gold_i = sample['answer']
-        pred_i = sample['parsed_pred']
-        if sample['question_type'] == 'multiple-choice':
+        gold_i = sample["answer"]
+        pred_i = sample["parsed_pred"]
+        if sample["question_type"] == "multiple-choice":
             correct = eval_multi_choice(gold_i, pred_i)
-        else: # open question
+        else:  # open question
             correct = eval_open(gold_i, pred_i)
 
         if correct:
-            judge_dict[sample['id']] = 'Correct'
+            judge_dict[sample["id"]] = "Correct"
             pred_correct += 1
         else:
-            judge_dict[sample['id']] = 'Wrong'
+            judge_dict[sample["id"]] = "Wrong"
 
     if len(samples) == 0:
-        return {'acc': 0}
-    return judge_dict, {'acc': pred_correct / len(samples)}
+        return {"acc": 0}
+    return judge_dict, {"acc": pred_correct / len(samples)}
+
+
 # DATA SAVING
 def save_json(filename, ds):
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         json.dump(ds, f, indent=4)
+
 
 def check_is_number(string):
     """
@@ -108,11 +119,12 @@ def check_is_number(string):
     https://github.com/MMMU-Benchmark/MMMU/blob/51ce7f3e829c16bb44bc5445782686b4c3508794/eval/eval_utils.py#L65
     """
     try:
-        float(string.replace(',', ''))
+        float(string.replace(",", ""))
         return True
     except ValueError:
         # check if there's comma inside
         return False
+
 
 def normalize_str(string):
     """
@@ -127,17 +139,18 @@ def normalize_str(string):
     is_number = check_is_number(string)
 
     if is_number:
-        string = string.replace(',', '')
+        string = string.replace(",", "")
         string = float(string)
         # leave 2 decimal
         string = round(string, 2)
         return [string]
-    else: # it's likely to be a string
-        # lower it 
+    else:  # it's likely to be a string
+        # lower it
         string = string.lower()
         if len(string) == 1:
-            return [" " + string, string + " "] # avoid trivial matches
+            return [" " + string, string + " "]  # avoid trivial matches
         return [string]
+
 
 def extract_numbers(string):
     """
@@ -145,11 +158,11 @@ def extract_numbers(string):
     https://github.com/MMMU-Benchmark/MMMU/blob/51ce7f3e829c16bb44bc5445782686b4c3508794/eval/eval_utils.py#L100
     """
     # Pattern for numbers with commas
-    pattern_commas = r'-?\b\d{1,3}(?:,\d{3})+\b'
+    pattern_commas = r"-?\b\d{1,3}(?:,\d{3})+\b"
     # Pattern for scientific notation
-    pattern_scientific = r'-?\d+(?:\.\d+)?[eE][+-]?\d+'
+    pattern_scientific = r"-?\d+(?:\.\d+)?[eE][+-]?\d+"
     # Pattern for simple numbers without commas
-    pattern_simple = r'-?(?:\d+\.\d+|\.\d+|\d+\b)(?![eE][+-]?\d+)(?![,\d])'
+    pattern_simple = r"-?(?:\d+\.\d+|\.\d+|\d+\b)(?![eE][+-]?\d+)(?![,\d])"
 
     # Extract numbers with commas
     numbers_with_commas = re.findall(pattern_commas, string)
@@ -161,14 +174,16 @@ def extract_numbers(string):
     # Combine all extracted numbersz
     all_numbers = numbers_with_commas + numbers_scientific + numbers_simple
     return all_numbers
+
+
 def get_multi_choice_info(options):
     """
     Given the list of options for multiple choice question
     Return the index2ans and all_choices
     https://github.com/MMMU-Benchmark/MMMU/blob/51ce7f3e829c16bb44bc5445782686b4c3508794/eval/data_utils.py#L54
     """
-    
-    start_chr = 'A'
+
+    start_chr = "A"
     all_choices = []
     index2ans = {}
     for i, option in enumerate(options):
@@ -177,50 +192,51 @@ def get_multi_choice_info(options):
 
     return index2ans, all_choices
 
+
 def parse_multi_choice_response(response, all_choices, index2ans):
     """
     Parse the prediction from the generated response.
     Return the predicted index e.g., A, B, C, D.
     https://github.com/MMMU-Benchmark/MMMU/blob/51ce7f3e829c16bb44bc5445782686b4c3508794/eval/eval_utils.py#L10
     """
-    for char in [',', '.', '!', '?', ';', ':', "'"]:
+    for char in [",", ".", "!", "?", ";", ":", "'"]:
         response = response.strip(char)
-    response = " " + response + " " # add space to avoid partial match
+    response = " " + response + " "  # add space to avoid partial match
 
     index_ans = True
     ans_with_brack = False
     candidates = []
     for choice in all_choices:  # e.g., (A) (B) (C) (D)
-        if f'({choice})' in response:
+        if f"({choice})" in response:
             candidates.append(choice)
             ans_with_brack = True
 
     if len(candidates) == 0:
-        for choice in all_choices: # e.g., A B C D
-            if f' {choice} ' in response:
+        for choice in all_choices:  # e.g., A B C D
+            if f" {choice} " in response:
                 candidates.append(choice)
 
     if len(candidates) == 0:
-        for choice in all_choices: # e.g., A. B. C. D.
-            if f'{choice}.' in response:
+        for choice in all_choices:  # e.g., A. B. C. D.
+            if f"{choice}." in response:
                 candidates.append(choice)
-                
+
     # if all above doesn't get candidates, check if the content is larger than 5 tokens and try to parse the example
     if len(candidates) == 0 and len(response.split()) > 5:
         for index, ans in index2ans.items():
             if ans.lower() in response.lower():
                 candidates.append(index)
-                index_ans = False # it's content ans.
+                index_ans = False  # it's content ans.
 
     if len(candidates) == 0:  # still not get answer, randomly choose one.
         pred_index = random.choice(all_choices)
     elif len(candidates) > 1:
         start_indexes = []
         if index_ans:
-            if ans_with_brack: 
+            if ans_with_brack:
                 for can in candidates:
-                    index = response.rfind(f'({can})')
-                    start_indexes.append(index) # -1 will be ignored anyway
+                    index = response.rfind(f"({can})")
+                    start_indexes.append(index)  # -1 will be ignored anyway
                 # start_indexes = [generated_response.index(f'({can})') for can in candidates]
             else:
                 for can in candidates:
@@ -232,10 +248,11 @@ def parse_multi_choice_response(response, all_choices, index2ans):
                 start_indexes.append(index)
         # get the last one
         pred_index = candidates[np.argmax(start_indexes)]
-    else: # if only one candidate, use it.
+    else:  # if only one candidate, use it.
         pred_index = candidates[0]
 
     return pred_index
+
 
 def parse_open_response(response):
     """
@@ -243,19 +260,19 @@ def parse_open_response(response):
     Return a list of predicted strings or numbers.
     https://github.com/MMMU-Benchmark/MMMU/blob/51ce7f3e829c16bb44bc5445782686b4c3508794/eval/eval_utils.py#L122
     """
+
     # content = content.strip("\n").strip(".").strip(" ")
     def get_key_subresponses(response):
         key_responses = []
         response = response.strip().strip(".").lower()
-        sub_responses = re.split(r'\.\s(?=[A-Z])|\n', response)
-        indicators_of_keys = ['could be ', 'so ', 'is ',
-                            'thus ', 'therefore ', 'final ', 'answer ', 'result ']
+        sub_responses = re.split(r"\.\s(?=[A-Z])|\n", response)
+        indicators_of_keys = ["could be ", "so ", "is ", "thus ", "therefore ", "final ", "answer ", "result "]
         key_responses = []
         for index, resp in enumerate(sub_responses):
             # if last one, accept it's an equation (the entire response can be just one sentence with equation)
             if index == len(sub_responses) - 1:
-                indicators_of_keys.extend(['='])
-            shortest_key_response = None # the shortest response that may contain the answer (tail part of the response)
+                indicators_of_keys.extend(["="])
+            shortest_key_response = None  # the shortest response that may contain the answer (tail part of the response)
             for indicator in indicators_of_keys:
                 if indicator in resp:
                     if not shortest_key_response:
@@ -264,18 +281,19 @@ def parse_open_response(response):
                         if len(resp.split(indicator)[-1].strip()) < len(shortest_key_response):
                             shortest_key_response = resp.split(indicator)[-1].strip()
                     # key_responses.append(resp.split(indicator)[1].strip())
-            
+
             if shortest_key_response:
                 # and it's not trivial
                 if shortest_key_response.strip() not in [":", ",", ".", "!", "?", ";", ":", "'"]:
                     key_responses.append(shortest_key_response)
-        if len(key_responses) == 0: # did not found any
+        if len(key_responses) == 0:  # did not found any
             return [response]
         return key_responses
+
     # pdb.set_trace()
     key_responses = get_key_subresponses(response)
 
-    pred_list = key_responses.copy() # keep the original string response
+    pred_list = key_responses.copy()  # keep the original string response
     for resp in key_responses:
         pred_list.extend(extract_numbers(resp))
 
@@ -298,8 +316,8 @@ def calculate_ins_level_acc(results: Dict):
     acc = 0
     ins_num = 0
     for cat_results in results.values():
-        acc += cat_results['acc'] * cat_results['num_example']
-        ins_num += cat_results['num_example']
+        acc += cat_results["acc"] * cat_results["num_example"]
+        ins_num += cat_results["num_example"]
     if ins_num == 0:
         return 0
     return acc / ins_num
@@ -327,7 +345,7 @@ class MMMUDataset(BaseEvalDataset):
         self.cur_datetime = utc_plus_8_time.strftime("%Y-%m-%d_%H-%M-%S")
         self.data = {}
         if subset == "all":
-            subsets =  get_dataset_config_names(data_path)
+            subsets = get_dataset_config_names(data_path)
             for subset in tqdm(subsets, desc="Loading subsets"):
                 self.data[subset] = load_dataset(data_path, subset, split=split, cache_dir=cache_dir, revision="main")
         else:
@@ -344,9 +362,8 @@ class MMMUDataset(BaseEvalDataset):
             letter = chr(65 + index)  # 65 is the ASCII value for 'A'
             parsed_result += f"{letter}. {option}\n"
         return parsed_result
-    
-    def _evaluate(self, model):
 
+    def _evaluate(self, model):
         model_version = model.name.split("/")[-1]
         result_path = os.path.join(self.default_output_path, f"{model_version}_{self.cur_datetime}_score.json")
         evaluation_result = {}
@@ -357,16 +374,18 @@ class MMMUDataset(BaseEvalDataset):
                 if data["image_2"] != None:
                     print(f"Warning: {data['id']} has more than one images.")
                     print(f"Skipping {data['id']} and set the score as zero")
-                    eval_samples.append({
-                        "id": data["id"],
-                        "question_type": data["question_type"],
-                        "parsed_pred": "other",
-                        "answer": data["answer"],
-                    })
+                    eval_samples.append(
+                        {
+                            "id": data["id"],
+                            "question_type": data["question_type"],
+                            "parsed_pred": "other",
+                            "answer": data["answer"],
+                        }
+                    )
                     continue
                 question = data["question"]
                 question = question.replace("<image 1>", "")
-                if data['question_type'] == 'multiple-choice':  
+                if data["question_type"] == "multiple-choice":
                     index2ans, all_choices = get_multi_choice_info(ast.literal_eval(data["options"]))
                     if self.multi_choice_prompt:
                         question = f"{self.multi_choice_prompt} {question}"
@@ -388,44 +407,40 @@ class MMMUDataset(BaseEvalDataset):
                     print(f"Raw prediction: {model_response}")
                     print(f"Parsed prediction: {pred_ans}\n")
 
-                eval_samples.append({
-                    "id": data["id"],
-                    "question_type": data["question_type"],
-                    "parsed_pred": pred_ans,
-                    "answer": data["answer"],
-                })
-            
+                eval_samples.append(
+                    {
+                        "id": data["id"],
+                        "question_type": data["question_type"],
+                        "parsed_pred": pred_ans,
+                        "answer": data["answer"],
+                    }
+                )
+
             judge_dict, metric_dict = evaluate_mmmu(eval_samples)
             metric_dict.update({"num_example": len(eval_samples)})
-        
+
             evaluation_result[category] = metric_dict
         evaluation_result[category] = metric_dict
-        
+
         printable_results = {}
         # pdb.set_trace()
         # add domain Subject
         for domain, in_domain_cats in DOMAIN_CAT2SUB_CAT.items():
             in_domain_cat_results = {}
-            for cat_name in in_domain_cats: # use the order in DOMAIN_CAT2SUB_CAT
+            for cat_name in in_domain_cats:  # use the order in DOMAIN_CAT2SUB_CAT
                 if cat_name in evaluation_result.keys():
                     in_domain_cat_results[cat_name] = evaluation_result[cat_name]
                 else:
                     pass
             in_domain_ins_acc = calculate_ins_level_acc(in_domain_cat_results)
-            in_domain_data_num = sum([cat_results['num_example'] for cat_results in in_domain_cat_results.values()])
-            printable_results['Overall-' + domain] = {"num": int(in_domain_data_num),
-                                                    "acc": round(in_domain_ins_acc, 3)
-                                                    }
+            in_domain_data_num = sum([cat_results["num_example"] for cat_results in in_domain_cat_results.values()])
+            printable_results["Overall-" + domain] = {"num": int(in_domain_data_num), "acc": round(in_domain_ins_acc, 3)}
             # add sub category
             for cat_name, cat_results in in_domain_cat_results.items():
-                printable_results[cat_name] = {"num": int(cat_results['num_example']),
-                                            "acc": round(cat_results['acc'], 3)
-                                            }
-            
+                printable_results[cat_name] = {"num": int(cat_results["num_example"]), "acc": round(cat_results["acc"], 3)}
+
         all_ins_acc = calculate_ins_level_acc(evaluation_result)
-        printable_results['Overall'] = {"num": sum([cat_results['num_example'] for cat_results in evaluation_result.values()]),
-                                        "acc": round(all_ins_acc, 3)
-                                        }
+        printable_results["Overall"] = {"num": sum([cat_results["num_example"] for cat_results in evaluation_result.values()]), "acc": round(all_ins_acc, 3)}
 
         print(printable_results)
         save_json(result_path, printable_results)
